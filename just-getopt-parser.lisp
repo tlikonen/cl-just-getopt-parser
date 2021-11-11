5b2b97dd38310252761d7503187da39047438a36
Teemu Likonen <tlikonen@iki.fi>
2021-11-11 17:45:52 +0200

    Fix typo in condition's documentation string

--- just-getopt-parser.lisp
+++ just-getopt-parser.lisp
@@ -34,7 +34,7 @@
   ()
   (:documentation
    "`getopt` function may signal this condition when it finds an unknown
-condition. Function `option-name` can be used to read option's name from
+option. Function `option-name` can be used to read option's name from
 the condition object.")
   (:report
    (lambda (condition stream)
